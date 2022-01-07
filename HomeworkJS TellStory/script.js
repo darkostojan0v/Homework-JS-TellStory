42bf23cbb4774f918246d596c75b8cf69789c4c4
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-01-07 16:59:14 +0100

    Add files via upload

---
+++ HomeworkJS TellStory/script.js
@@ -0,0 +1,9 @@
+console.log('Tell a Story()');
+
+function tellAStory() {
+    let firstName = 'Darko';
+    let mood = 'happy';
+    let activity = 'working out';
+    return `This is ${firstName}. ${firstName} is a nice person. Today he is ${mood}. He has been ${activity} all day. The end.`;
+}
+console.log(tellAStory());
